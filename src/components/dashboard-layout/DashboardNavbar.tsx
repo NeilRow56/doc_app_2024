4d358d751a5fe0555cf90f7f04be442334b924fb
NeilRow56 <neilrowland56@gmail.com>
2024-03-22 06:59:24 +0000

    Updated dashboard navbar

--- src/components/dashboard-layout/DashboardNavbar.tsx
+++ src/components/dashboard-layout/DashboardNavbar.tsx
@@ -1,5 +1,20 @@
+'use client'
+
 import { Button } from '../ui/button'
 import Link from 'next/link'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+
+import { Search } from 'lucide-react'
+import { Input } from '../ui/input'
+import { ThemeToggle } from './ThemeToggle'
 
 function DashboardNavbar() {
   // Holding userId ready for auth
@@ -15,21 +30,63 @@ function DashboardNavbar() {
   else if (hrs >= 17 && hrs <= 24) greet = 'Good Evening!'
 
   return (
-    <div className="flex h-14 w-full items-center border border-gray-600  bg-white  px-2 dark:bg-neutral-950 md:px-12  lg:px-48">
-      <div className="justify-cstart flex flex-1">
+    <div className="flex h-16 w-full items-center justify-between  border  border-gray-600  bg-white px-2 dark:bg-neutral-950  md:px-12 lg:px-48">
+      <div className="">
         <div>
           <p className="text-xl text-primary">{greet}</p>
         </div>
       </div>
-      <div className=" flex  gap-6">
+      <div className="relative hidden sm:block ">
+        <Search className="absolute left-4 top-3 h-4 w-4 text-muted-foreground" />
+        <Input
+          placeholder="Search"
+          name="search"
+          className="primary/40 rounded-lg pl-10 dark:border-gray-300"
+        />
+      </div>
+      <div className=" flex items-center justify-center  gap-6">
+        <ThemeToggle />
         {userId ? (
-          <Button asChild size="sm" className="px-6  ">
-            <Link href="/sign-in">{userId}</Link>
-          </Button>
+          <div className="flex w-full">
+            <div className=" space-x-2">
+              <DropdownMenu>
+                <DropdownMenuTrigger className=" text-primary">
+                  <Avatar className="mt-2 h-12 w-12">
+                    <AvatarImage src="/profile.jpg" />
+                    <AvatarFallback>DR</AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>
+                    <Avatar className="h-12 w-12 ">
+                      <AvatarImage src="/profile.jpg" />
+                      <AvatarFallback>DR</AvatarFallback>
+                    </Avatar>
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <Link href="/hotel/new">New</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Link href="/edgestore">edgestore</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>Team</DropdownMenuItem>
+                  <DropdownMenuItem>Subscription</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+            <div className="pt-4">
+              <Button asChild size="sm" className="ml-4  px-6 ">
+                <Link href="/sign-in">{userId}</Link>
+              </Button>
+            </div>
+          </div>
         ) : (
-          <Button asChild size="sm" className="px-6  ">
-            <Link href="/sign-in">Login</Link>
-          </Button>
+          <div>
+            <Button asChild size="sm" className="px-6  ">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </div>
         )}
       </div>
     </div>
